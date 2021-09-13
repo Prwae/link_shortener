4776cba8e2967c9dc2b088fb8b28538de9f58e24
Prytkovkach <75357629+Prwae@users.noreply.github.com>
2021-09-13 18:27:10 +0300

    Высушил названия
    Сделал более точные названия переменных

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import os
 import argparse
 
 
-def is_bitlink(link, head):
-  url = f"https://api-ssl.bitly.com/v4/bitlinks/{link}"
+def is_bitlink(url_link, head):
+  url = f"https://api-ssl.bitly.com/v4/bitlinks/{url_link}"
   response = requests.get(url, headers=head)
   return response.ok
 
 
-def shorten_link(head, user_link):
+def shorten_link(head, url_link):
   body = {
-    "long_url": user_link
+    "long_url": url_link
   }
 
   url = "https://api-ssl.bitly.com/v4/bitlinks"
@@ -39,19 +39,19 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser(
     description='Сокращение ссылок'
   )
-  parser.add_argument('user_link', help='Ссылка для сокращения или битлинк для получения информации о нем')
-  user_url = parser.parse_args().user_link
+  parser.add_argument('link', help='Ссылка для сокращения или битлинк для получения информации о нем')
+  link = parser.parse_args().link
 
-  parsed_link = urlparse(user_url)
-  user_url_without_scheme = f"{parsed_link.netloc}{parsed_link.path}"
+  parsed_link = urlparse(link)
+  link_without_scheme = f"{parsed_link.netloc}{parsed_link.path}"
 
-  if is_bitlink(user_url_without_scheme, headers):
+  if is_bitlink(link_without_scheme, headers):
     try:
-      print("Кликов всего: ", count_clicks(user_url_without_scheme, headers))
+      print("Кликов всего: ", count_clicks(link_without_scheme, headers))
     except requests.exceptions.HTTPError:
       print("Ошибка при подсчете кликов")
   else:
     try:
-      print("Битлинк", shorten_link(headers, user_url))
+      print("Битлинк", shorten_link(headers, link))
     except requests.exceptions.HTTPError:
       print("Ошибка при сокращении ссылки")
